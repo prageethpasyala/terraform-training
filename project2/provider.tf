dad5a68dcaca56bcafb9ceff1d92a6b69cab2479
PK <93587237+prageethpasyala@users.noreply.github.com>
2021-11-25 22:29:53 +0000

    delete

--- project2/provider.tf
+++
@@ -1,9 +0,0 @@
-# provider "aws" {
-#   region = "eu-west-1"
-#   access_key = "AKIAUSHUKNLM6YI3OPEF"
-#   secret_key = "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"
-
-# }
-
-# Access key : AKIAUSHUKNLM6YI3OPEF
-# Secrect key : vOjqRzDA9bRwVlRaaABCurA+RkJ3PO5ixz5E45Ri
